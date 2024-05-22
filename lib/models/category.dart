f9498180bbd6f69d3f4ecb7393dbdbf3a02c81b0
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-05-22 15:39:43 +0300

    refactor: Update Category model to include ID

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,9 +1,11 @@
 class Category {
   final String name;
+  final int id;
 
-  Category({required this.name});
+  Category({required this.name, required this.id});
 
   factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(name: json['CategoryName']);
+    return Category(
+        name: json['CategoryName'] as String, id: json['CategoryID'] as int);
   }
 }
