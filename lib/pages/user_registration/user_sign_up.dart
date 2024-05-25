9a8dbec6d6bebfa748addbd0c5ec8dfebba34b55
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-05-25 22:44:56 +0300

    feat: Check if user already exists before signing up

--- lib/pages/user_registration/user_sign_up.dart
+++ lib/pages/user_registration/user_sign_up.dart
@@ -51,9 +51,57 @@ class _UserSignUp extends State<UserSignUp> {
     }
   }
 
+  void _isUserExist(String email) async {
+    const url = 'http://localhost:5074/api/User';
+    final uri = Uri.parse(url);
+
+    try {
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonResponse = jsonDecode(response.body);
+
+        List<dynamic> _users = jsonResponse;
+
+        bool isUserFound = false;
+        for (var user in _users) {
+          if (user is Map) {
+            String? userEmail = user['userEmail'];
+
+            if (userEmail == email) {
+              isUserFound = true;
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('User already exists'),
+                  backgroundColor: Theme.of(context).colorScheme.error,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                ),
+              );
+              break;
+            }
+          }
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to check user existence: $e'),
+          backgroundColor: Theme.of(context).colorScheme.error,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ),
+      );
+    }
+  }
+
   void _signUp(
       String name, String surname, String email, String password) async {
     if (_formKey.currentState?.validate() ?? false) {
+      _isUserExist(email);
+
       final _name = name;
       final _surname = surname;
       final _email = email;
