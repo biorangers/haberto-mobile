8acb04e27a182c463df6947098da19ad9bd09e52
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-05-28 19:52:07 +0300

    feat: Add search functionality and API integration to navbar widget

--- lib/widgets/navbar_widget.dart
+++ lib/widgets/navbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haberto_mobile/pages/search_page/search_page.dart';
 
 class NavbarWidget extends StatefulWidget {
   const NavbarWidget({super.key});
@@ -11,9 +12,28 @@ class _NavbarWidgetState extends State<NavbarWidget> {
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
+    print('INDEX: ' + index.toString());
     setState(() {
       _selectedIndex = index;
     });
+    switch (index) {
+      case 0:
+        // Ana sayfa
+        break;
+      case 1:
+        print('INDEX: ' + index.toString());
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SearchPage()),
+        );
+        break;
+      case 2:
+        // Kaydedilenler
+        break;
+      case 3:
+        // Profil
+        break;
+    }
   }
 
   @override
